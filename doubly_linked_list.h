a1e5319235cb6588f6ca04c247bee20429b3d00a
Youssef Hassan <yousefheroo028@gmail.com>
2024-12-30 02:50:09 +0200

    Add files via upload

--- doubly_linked_list.h
+++ doubly_linked_list.h
@@ -25,6 +25,16 @@ public:
         head = tail = nullptr;
     }
 
+    T front()
+    {
+        return head->value;
+    }
+
+    T back()
+    {
+        return tail->value;
+    }
+
     void insert_back(T item)
     {
         Node* newItem = new Node;
@@ -62,26 +72,27 @@ public:
         length++;
     }
 
-    void insert_in_pos(T item, const int pos)
+    void insert_in_pos(T item, int pos)
     {
         if (pos == 0) insert_front(item);
         else if (pos == length) insert_back(item);
-        else if (pos < 0 || pos > length) throw out_of_range("Position out of range");
-        else
+        while (pos < 0 || pos > length)
         {
-            Node* newItem = new Node;
-            newItem->value = item;
-            Node* current = head;
-            for (int i = 0; i < pos - 1; i++)
-            {
-                current = current->next;
-            }
-            newItem->next = current->next;
-            newItem->previous = current;
-            current->next = newItem;
-            current->next->previous = newItem;
-            length++;
+            cout << "Please insert at index within [0 - " << length - 1 << "]: ";
+            cin >> pos;
         }
+        Node* newItem = new Node;
+        newItem->value = item;
+        Node* current = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            current = current->next;
+        }
+        newItem->next = current->next;
+        newItem->previous = current;
+        current->next = newItem;
+        current->next->previous = newItem;
+        length++;
     }
 
     void remove_front()
@@ -106,6 +117,18 @@ public:
             length--;
         }
         else if (length == 1) tail = nullptr, length--;
+    }
+
+    int count(T element)
+    {
+        int counter = 0;
+        Node* current = head;
+        for (int i = 0; i < length; i++)
+        {
+            if (current->value == element) counter++;
+            current = current->next;
+        }
+        return counter;
     }
 
     void remove_from_pos(const int pos)
@@ -136,31 +159,85 @@ public:
         return !length;
     }
 
+    int is_exist(T element)
+    {
+        Node* current = head;
+        for (int i = 0; i < length; i++)
+        {
+            if (current->value == element)
+            {
+                return i;
+            }
+            current = current->next;
+        }
+        return -1;
+    }
+
+    pair<T, int> max_element()
+    {
+        int max = INT_MIN, index = 0;
+        Node* current = head;
+        for (int i = 0; i < length; i++)
+        {
+            if (current->value > max) max = current->value, index = i;
+            current = current->next;
+        }
+        return make_pair(max, index);
+    }
+
+    pair<T, int> min_element()
+    {
+        int min = INT_MAX, index = 0;
+        Node* current = head;
+        for (int i = 0; i < length; i++)
+        {
+            if (current->value < min) min = current->value, index = i;
+            current = current->next;
+        }
+        return make_pair(min, index);
+    }
+
+    void modify_at_pos(const int pos, T item)
+    {
+        Node* current = head;
+        for (int i = 0; i < pos; i++)
+        {
+            current = current->next;
+        }
+        current->value = item;
+    }
+
+    void clear()
+    {
+        head = tail = nullptr, length = 0;
+    }
+
     int size() const
     {
         return length;
     }
 
-    void print()
+    friend ostream& operator<<(ostream& print, doubly_linked_list<T>& list)
     {
-        if (!length)
+        if (!list.size())
         {
-            cout << "The list is empty.\n";
+            print << "The list is empty.\n";
         }
         else
         {
-            Node* current = head;
-            int temp = length - 1;
-            cout << "List: {";
-            cout << current->value;
+            Node* current = list.head;
+            int temp = list.size() - 1;
+            print << "List: {";
+            print << current->value;
             current = current->next;
             while (temp--)
             {
-                cout << ", " << current->value;
+                print << ", " << current->value;
                 current = current->next;
             }
-            cout << "}\n";
+            print << "}";
         }
+        return print;
     }
 
     ~doubly_linked_list()
